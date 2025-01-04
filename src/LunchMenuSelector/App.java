3b6e91128f2a3a90dd2f309610c9f39f2b5b28b1
JeminPARK <winde7249@naver.com>
2025-01-04 21:36:36 +0900

    lunch menu selector start

---
+++ src/LunchMenuSelector/App.java
@@ -0,0 +1,16 @@
+package LunchMenuSelector;
+
+/**
+ * 
+ * Q :   바쁜 직장인들을 위해 미리 선정된 점심 메뉴를 임의로 선택하여 제공하는 프로그램.
+ *       1. 미리 선정된 메뉴를 사용하여 임의로 제공.     
+ *       2. 사용자가 직접 메뉴를 입력하여 임의의 메뉴가 선택될 수 있도록 제공.
+ * 
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
