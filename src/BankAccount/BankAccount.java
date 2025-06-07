e621fa876a3c9d24597b563c98acc8197b597ab8
JeminPARK <JeminPARK@jm.com>
2025-06-07 21:06:09 +0900

    Bank Management Start

--- src/BankAccount/BankAccount.java
+++ src/BankAccount/BankAccount.java
@@ -14,4 +14,30 @@ public class BankAccount {
         this.onwerName = onwerName;
         this.balance = balance;
     }
+
+    //메서드
+    public void deposit (double amount) {
+        if(amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw (double amount) {
+        if(amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+        else {
+            System.out.println("잔액 부족 또는 잘못된 금액입니다.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void printAccountInfo() {
+        System.out.println("계좌번호: " + accountNumber);
+        System.out.println("소유자: " + onwerName);
+        System.out.println("잔액: " + balance + " 원");
+    }
 }
