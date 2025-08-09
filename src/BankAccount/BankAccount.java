683acef658c979ddde2112350456369c61285ae5
JeminPARK <winde7249@naver.com>
2025-08-09 23:32:00 +0900

    BankApp Test

--- src/BankAccount/BankAccount.java
+++ src/BankAccount/BankAccount.java
@@ -4,9 +4,10 @@ public class BankAccount {
     // 은행 계좌를 관리하는 클래스
     // 계좌번호, 계좌주, 잔액
     
-    private String accountNumber;
-    private String onwerName;
-    private double balance;
+    //1. 필드 (계좌 속성)
+    private String accountNumber; // 계좌번호
+    private String onwerName;     // 계좌주 이름
+    private double balance;       // 잔액
 
     // 생성자
     public BankAccount(String accountNumber, String onwerName, double balance) {
@@ -15,29 +16,38 @@ public class BankAccount {
         this.balance = balance;
     }
 
-    //메서드
+    // 입금 메서드
     public void deposit (double amount) {
         if(amount > 0) {
             balance += amount;
+            System.out.println(amount + " 원이 입금되었습니다.");
+        }
+        else {
+            System.out.println("0원 이상 입금 가능합니다.");
         }
     }
 
+    // 출금 메서드
     public void withdraw (double amount) {
         if(amount > 0 && amount <= balance) {
             balance -= amount;
+            System.out.println(amount + " 원이 출금되었습니다.");
         }
         else {
             System.out.println("잔액 부족 또는 잘못된 금액입니다.");
         }
     }
 
+    // 잔액 조회 메서드
     public double getBalance() {
         return balance;
     }
 
+    // 계좌 정보 출력 메서드
     public void printAccountInfo() {
         System.out.println("계좌번호: " + accountNumber);
         System.out.println("소유자: " + onwerName);
         System.out.println("잔액: " + balance + " 원");
+        System.out.println("-------------------------");
     }
 }
--- src/BankAccount/BankApp.java
+++ src/BankAccount/BankApp.java
@@ -5,11 +5,25 @@ public class BankApp {
     public void run() {
 
         BankAccount account1 = new BankAccount("123-456", "홍길동", 1000000);
-    
+        
+        // 계좌 정보 출력
+        System.out.println("계좌 정보 출력");
         account1.printAccountInfo();
+
+        // 입금 및 출금 테스트
+        System.out.println("입금 및 출금 테스트");
+        System.out.println("잔액" + account1.getBalance() + " 원");
+
+        // 입금
         account1.deposit(50000);
         System.out.println("잔액" + account1.getBalance() + " 원");
+
+        // 출금
         account1.withdraw(200000);
         System.out.println("잔액" + account1.getBalance() + " 원");
+
+        // 최종 계좌 정보 출력
+        System.out.println("최종 계좌 정보 출력");
+        account1.printAccountInfo();
     }
 }
